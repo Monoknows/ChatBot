cc2136427550aa01a84ea7075bbb8f90ed60be3b
Monoknows <jon.bernabe@email.lcup.edu.ph>
2026-01-05 14:12:41 +0800

    fix?

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Chatbot from "./components/Chatbot/Chatbot";
 import LandingPage from "./components/LandingPage/LandingPage";
